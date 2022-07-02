8a7656f1441297ee3c0d9f21cd8b36b451c20533
Victor Gabriel <gabrielvghs666@gmail.com>
2022-07-02 20:51:59 -0300

    refactor: add new refactor for the archives

--- src/components/ButtonEnter.jsx
+++ src/components/ButtonEnter.jsx
@@ -1,11 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { userLoginStorage } from '../helpers/sendDataStorage';
+
 export const ButtonEnter = () => {
   const location = useLocation();
+  const { userReducer } = useSelector((state) => state);
+
+  function sendDataStorage() {
+    userLoginStorage(userReducer.id, userReducer.name);
+  }
+
   return (
     <NavLink
       to={`${location.pathname === '/' ? '/signup' : '/monthly-income'}`}
       className="buttonGo"
+      onClick={() => location.pathname === '/signup' && sendDataStorage()}
     >
       Começar
     </NavLink>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,32 @@
 import { ButtonEnter } from './ButtonEnter';
 import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setName, setPassword } from '../Redux/slices/userSlice';
 
 export const Form = () => {
+  const dispatch = useDispatch();
+  
+
+  function handleNameUser({ target }) {
+    dispatch(setName(target.value));
+  }
+
+  function handlePassword({target}) {
+    dispatch(setPassword(target.value))
+  }
+
   return (
     <section className="bg-white h-80 rounded-t-[35px] ">
-      <form className="">
+      <form>
         <div className=" my-8 mx-3">
           <input
+            onChange={handleNameUser}
             type="text"
             placeholder="Nome"
             className="mb-3 p-1 outline-none text-lg border-b-2 w-full"
           />
           <input
+            onChange={handlePassword}
             type="password"
             placeholder="Senha"
             className="mt-2 p-1 outline-none text-lg border-b-2 w-full"
